80ad5b9252586ae08ef3da4592e55417dc1c57e9
Valentin <valentinpaes@cjr.org.br>
2018-11-22 17:03:01 +0000

    Homepage finalizada

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_144327) do
+ActiveRecord::Schema.define(version: 2018_11_22_161240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "test_meals", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
