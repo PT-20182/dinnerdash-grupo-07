18c67be4649ab61f06ce95b86e2f4d990738336c
Oscar2019 <170112209@aluno.unb.br>
2018-11-23 11:49:44 -0200

    CRUD pronto

---
+++ app/controllers/meal_categories_controller.rb
@@ -0,0 +1,43 @@
+class MealCategoriesController < ApplicationController
+    def index
+        @meal_categories = MealCategory.all
+    end
+
+    def new
+        @meal_category = MealCategory.new
+    end
+
+    def create
+        meal_category = MealCategory.create(meal_category_params)
+        redirect_to meal_categories_path
+    end
+
+    def show
+        @meal_category = MealCategory.find(params[:id])
+    end
+
+    def edit
+        @meal_category = MealCategory.find(params[:id])
+    end
+
+    def update
+        @meal_category = MealCategory.find(params[:id])
+        @meal_category.update(meal_category_params)
+
+
+        redirect_to meal_category_path(@meal_category)
+    end
+
+    def destroy
+        @meal_category = MealCategory.find(params[:id])
+        @meal_category.destroy
+
+        redirect_to meal_categories_path
+    end
+
+    private
+
+    def meal_category_params
+        params.require(:meal_category).permit(:name)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MealCategory.create(name: 'Massa')
+MealCategory.create(name: 'Salada')
+MealCategory.create(name: 'Entrada')
+MealCategory.create(name: 'Parto principal')
+MealCategory.create(name: 'Sobremesa')
