55b9dbb4d1a3237eb8e5b8dd1e5ea11fcce6e757
Oscar2019 <170112209@aluno.unb.br>
2018-12-02 13:26:37 -0200

    editar cateforia de refeiçao feito  com sucesso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_113425) do
+ActiveRecord::Schema.define(version: 2018_11_26_154456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 2018_11_23_113425) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "test_meals", force: :cascade do |t|
+  create_table "meals", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "price"
+    t.float "price"
+    t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
