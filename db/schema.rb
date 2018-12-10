033927e65dcec50d0f33549137953fc21a5f3819
Valentin <valentinpaes@cjr.org.br>
2018-12-10 04:46:21 +0000

    corrigido um problema nos links da pagina de meals

--- db/schema.rb
+++ db/schema.rb
@@ -30,14 +30,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_154456) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "test_meals", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
