2390d58deb394f5ccb6162a02bfde339ffe3f1dd
matheusrg21 <matheusrg21@hotmail.com>
2018-12-11 14:47:19 -0200

    Corrigindo funcionalidade de quantidade de cada prato

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,51 +2,34 @@ class CartController < ApplicationController
     @@preco_pedido = 0
     def add_item
         @preco_total
+        @qtd_meal = params[:qtd].to_i
         @meal_id = params[:id]
-        qtd = []
+        qtd_preco = []
+
         set_cart.each do |index|
             if(index.has_key?(@meal_id))
-                qtd[@meal_id.to_i] = index[@meal_id][0] + 1
-                print '&&&&&&&&&&&& '
-                puts 'Refeição já presente'
+                qtd_preco[@meal_id.to_i] = index[@meal_id][0] + @qtd_meal
             end
             if index[@meal_id].nil?
-                qtd[@meal_id.to_i] = 1
-                print '@@@@@@@@@@@@@ '
-                puts 'NOVO ID'
+                qtd_preco[@meal_id.to_i] = @qtd_meal
             end
         end
 
         if (set_cart.empty?)
-            print '$$$$$$$$$$$$$$$ '
-            puts 'Carrinho vazio'
-            qtd[@meal_id.to_i] = 1
+            qtd_preco[@meal_id.to_i] = @qtd_meal
         end
 
-        @preco_total = calcula_preco_total(qtd[@meal_id.to_i])
+        @preco_total = calcula_preco_total(qtd_preco[@meal_id.to_i])
 
         if(set_cart[0].nil?)
-            print '++++++++++++++ '
-            puts 'Carrinho nulo'
-            set_cart.push({@meal_id => [qtd[@meal_id.to_i], @preco_total]})
+            set_cart.push({@meal_id => [qtd_preco[@meal_id.to_i], @preco_total]})
         end
 
         if(set_cart[0].has_key?(@meal_id) == false)
-            set_cart[0][@meal_id] = [qtd[@meal_id.to_i], @preco_total]
-            # set_cart[0].fetch(@meal_id, [qtd[@meal_id.to_i], @preco_total])
-            print '############### '
-            puts 'NOVO ID ADC'
-            print 'cart '
-            puts set_cart.to_s
+            set_cart[0][@meal_id] = [qtd_preco[@meal_id.to_i], @preco_total]
         else
-            set_cart[0][@meal_id] = [qtd[@meal_id.to_i], @preco_total]
+            set_cart[0][@meal_id] = [qtd_preco[@meal_id.to_i], @preco_total]
         end
-
-        set_cart.each do |index|
-            print '!!!!!!!!!!!!!!!!!!! '
-            puts index[@meal_id][1]
-        end
-        
 
         redirect_to root_path
     end
